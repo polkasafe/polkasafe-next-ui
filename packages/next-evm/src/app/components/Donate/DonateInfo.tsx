1fedacdc33e942fb4ed2de5dc0e0059b58fe1a4a
aadarsh012 <aadarsh012@gmail.com>
2024-02-04 18:07:22 +0530

    Donate view changed

--- packages/next-evm/src/app/components/Donate/DonateInfo.tsx
+++ packages/next-evm/src/app/components/Donate/DonateInfo.tsx
@@ -29,24 +29,24 @@ const DonateInfo = () => {
 			</div>
 			<Divider className='bg-text_secondary my-8' />
 			<div className='flex items-center gap-x-5 gap-y-5 flex-col md:flex-row justify-between text-white'>
-				<p className='flex gap-x-2 flex-col xl:flex-row xl:items-center'>
-					<span className='text-sm font-normal mb-2'>
-						If you don&apos;t have a polkadot wallet, don&apos;t worry, you can create one for free at
+				<p className='flex gap-x-2 items-center'>
+					<span className='text-sm font-normal'>
+						If you don&apos;t have a wallet, don&apos;t worry, you can create one for free at
 					</span>
 					<span className='text-primary'>
 						<a
-							href='https://polkadot.js.org/extension/'
+							href='https://metamask.io/'
 							target='_blank'
 							rel='noreferrer'
 							className='flex items-center'
 						>
-							Polkadot{'{.js}'}
+							Metamask
 							<ExternalLinkIcon className='text-lg ml-2' />
 						</a>
 					</span>
 				</p>
 				<a
-					href='https://polkadot.js.org/extension/'
+					href='https://metamask.io/'
 					rel='noreferrer'
 					target='_blank'
 					className='text-primary px-[10px] py-2 bg-highlight rounded-lg flex items-center border-none outline-none gap-x-[10.83px]'
--- packages/next-evm/src/app/components/Donate/QR.tsx
+++ packages/next-evm/src/app/components/Donate/QR.tsx
@@ -7,30 +7,30 @@ import AddressQr from '@next-common/ui-components/AddressQr';
 import { CopyIcon, ExternalLinkIcon } from '@next-common/ui-components/CustomIcons';
 import copyText from '@next-evm/utils/copyText';
 import shortenAddress from '@next-evm/utils/shortenAddress';
+import { NETWORK, chainProperties } from '@next-common/global/evm-network-constants';
 
 const QR = () => {
-	const { network } = useGlobalApiContext();
 	return (
 		<div className='flex flex-col gap-y-5 p-5 bg-bg-secondary rounded-xl items-center'>
 			<p className='text-xs md:text-sm text-normal text-text_secondary'>
 				Scan this QR Code with your wallet application
 			</p>
 			<div className='flex items-center justify-center'>
-				<AddressQr address='165gUhnbTdZEfjY4drYNybJuRBf3MLJfZxQUraJDeX17B4Pb' />
+				<AddressQr address='0xa6f1f10E2d415366ED1912869CF14a1E29Df0c09' />
 			</div>
 			<div className='flex items-center gap-x-3 justify-center bg-highlight rounded-lg p-2'>
 				<p className='text-xs md:text-sm leading-[15px]'>
-					<span className='text-primary font-medium'>dot:</span>
+					<span className='text-primary font-medium'>ETH:</span>
 					<span className='font-normal ml-[6px]'>
-						{shortenAddress('165gUhnbTdZEfjY4drYNybJuRBf3MLJfZxQUraJDeX17B4Pb')}
+						{shortenAddress('0xa6f1f10E2d415366ED1912869CF14a1E29Df0c09')}
 					</span>
 				</p>
 				<p className='text-sm md:text-base text-text_secondary flex items-center gap-x-[9px]'>
-					<button onClick={() => copyText('165gUhnbTdZEfjY4drYNybJuRBf3MLJfZxQUraJDeX17B4Pb')}>
+					<button onClick={() => copyText('0xa6f1f10E2d415366ED1912869CF14a1E29Df0c09')}>
 						<CopyIcon className='hover:text-primary' />
 					</button>
 					<a
-						href={`https://${network}.subscan.io/account/165gUhnbTdZEfjY4drYNybJuRBf3MLJfZxQUraJDeX17B4Pb`}
+						href={`${chainProperties[NETWORK.ETHEREUM].blockExplorer}/address/0xa6f1f10E2d415366ED1912869CF14a1E29Df0c09`}
 						target='_blank'
 						rel='noreferrer'
 					>
