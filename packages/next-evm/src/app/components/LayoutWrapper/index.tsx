6f7caddf178c39e4b5b3b68d43a0d0cc3390e91d
aadarsh012 <aadarsh012@gmail.com>
2024-02-07 03:33:47 +0530

    disabled for small devices

--- packages/next-evm/src/app/components/LayoutWrapper/index.tsx
+++ packages/next-evm/src/app/components/LayoutWrapper/index.tsx
@@ -10,6 +10,8 @@ const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
 	const { ready, authenticated } = usePrivy();
 	const router = useRouter();
 
+	const disabled = typeof window !== 'undefined' && window.innerWidth <= 800;
+
 	useEffect(() => {
 		if (ready && !!router.push) {
 			setIsLayoutReady(true);
@@ -20,7 +22,20 @@ const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
 		}
 	}, [authenticated, ready, router]);
 
-	return isLayoutReady ? children : <SplashScreen />;
+	return disabled ? (
+		<div className='h-screen w-full flex justify-center items-start p-8 bg-bg-main'>
+			<section className='mb-4 text-sm border-2 border-solid border-waiting w-full text-waiting bg-waiting bg-opacity-10 p-2.5 rounded-lg flex items-center gap-x-2'>
+				<p className='text-white'>
+					Sorry, this website is not accessible on mobile devices. Please visit us on a desktop or laptop computer for
+					the best experience.
+				</p>
+			</section>
+		</div>
+	) : isLayoutReady ? (
+		children
+	) : (
+		<SplashScreen />
+	);
 };
 
 export default LayoutWrapper;
