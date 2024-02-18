a7c09cb121a6ae3f9ed8cdaeaeea3ff2fe2a3d6f
aadarsh012 <aadarsh012@gmail.com>
2024-02-19 00:22:57 +0530

    bug fix

--- packages/next-common/global/subscan_consts.ts
+++ packages/next-common/global/subscan_consts.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 /* eslint-disable @typescript-eslint/naming-convention */
-export const SUBSCAN_API_KEY = '056b677410ac226bea971a3e03de66fa';
+export const SUBSCAN_API_KEY = '11bf7d27e79d46e682ac0dc1abaf0013';
 
 export const SUBSCAN_API_HEADERS = {
 	Accept: 'application/json',
