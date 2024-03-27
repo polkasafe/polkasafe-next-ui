d01aa13dbaa47d6480f87c9af43db25b8658c7a2
Ellu <aleem.alam@outlook.com>
2024-03-27 09:13:52 +0530

    update create proposal

---
+++ packages/next-common/ui-components/HelperTooltip.tsx
@@ -0,0 +1,31 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
+import { TooltipPlacement } from 'antd/es/tooltip';
+import React, { ReactNode } from 'react';
+
+interface Props {
+	className?: string;
+	text: string | ReactNode;
+	bgColor?: string;
+	placement?: TooltipPlacement;
+}
+
+const HelperTooltip = ({ className, text, bgColor = 'rgb(21 115 254 / var(--tw-text-opacity))', placement }: Props) => {
+	return (
+		<Tooltip
+			placement={placement}
+			color={bgColor}
+			title={text}
+			getPopupContainer={(triggerNode) => triggerNode}
+			className='text-primary'
+		>
+			<InfoCircleOutlined className={className} />
+		</Tooltip>
+	);
+};
+
+export default HelperTooltip;
