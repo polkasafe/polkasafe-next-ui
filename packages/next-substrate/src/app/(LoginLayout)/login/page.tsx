8400c2a9d10cb4411ff9eebcac9d41b781f03025
aadarsh012 <aadarsh012@gmail.com>
2024-04-17 02:29:31 +0530

    added addresses to whitelist

--- packages/next-substrate/src/app/(LoginLayout)/login/page.tsx
+++ packages/next-substrate/src/app/(LoginLayout)/login/page.tsx
@@ -32,7 +32,9 @@ const whitelist = [
 	getSubstrateAddress('15s78GDxmAhxNdt6pxaxGcPrzboaMem5k3jP3xXyZvVVfLLr'),
 	getSubstrateAddress('15kAhLvVhtQuWyMDvts3pPAbz3maLbz7CSdwcbg5UQ96GATt'),
 	getSubstrateAddress('15Sf9AnqDooBgdV91hixPHY99SJom9DMzLKbxg6dYRsqTa4a'),
-	getSubstrateAddress('5Gq84otocj45uGWqB4cacNnVeyCCFeKHg6EtK76BLvh2sM1s')
+	getSubstrateAddress('5Gq84otocj45uGWqB4cacNnVeyCCFeKHg6EtK76BLvh2sM1s'),
+	getSubstrateAddress('13rvrw9GrtYAEkDbttj4Js5VfYUN9vgGQhoygEfW39Ug6dFp'),
+	getSubstrateAddress('15rEKyJt1WV7SUEK1N5ddEBRDCZPewakwVdgb2Nzk4cvuoGD')
 ];
 
 // eslint-disable-next-line sonarjs/cognitive-complexity
