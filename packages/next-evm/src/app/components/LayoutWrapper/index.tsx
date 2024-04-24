25de008b818119f4c0dc32856e0208f3cfb80bdb
AleemAlam <aleem.alam@outlook.com>
2024-04-24 10:27:26 +0200

    hot fixes

--- packages/next-evm/src/app/components/LayoutWrapper/index.tsx
+++ packages/next-evm/src/app/components/LayoutWrapper/index.tsx
@@ -10,7 +10,7 @@ const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
 	const { ready, authenticated } = usePrivy();
 	const router = useRouter();
 
-	const disabled = typeof window !== 'undefined' && window.innerWidth <= 800;
+	// const disabled = typeof window !== 'undefined' && window.innerWidth <= 800;
 
 	useEffect(() => {
 		if (ready && !!router.push) {
@@ -22,16 +22,7 @@ const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
 		}
 	}, [authenticated, ready, router]);
 
-	return !disabled ? (
-		<div className='h-screen w-full flex justify-center items-start p-8 bg-bg-main'>
-			<section className='mb-4 text-sm border-2 border-solid border-waiting w-full text-waiting bg-waiting bg-opacity-10 p-2.5 rounded-lg flex items-center gap-x-2'>
-				<p className='text-white'>
-					Sorry, this website is not accessible on mobile devices. Please visit us on a desktop or laptop computer for
-					the best experience.
-				</p>
-			</section>
-		</div>
-	) : isLayoutReady ? (
+	return isLayoutReady ? (
 		children
 	) : (
 		<SplashScreen />
