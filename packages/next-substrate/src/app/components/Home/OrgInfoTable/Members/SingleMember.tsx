79831509adcc2d06b91c007cfba6e15289b31b1d
aadarsh012 <aadarsh012@gmail.com>
2024-04-26 01:08:19 +0530

    fixes

--- packages/next-substrate/src/app/components/Home/OrgInfoTable/Members/SingleMember.tsx
+++ packages/next-substrate/src/app/components/Home/OrgInfoTable/Members/SingleMember.tsx
@@ -1,9 +1,12 @@
 import { IAllAddresses } from '@next-common/types';
 import { useActiveOrgContext } from '@next-substrate/context/ActiveOrgContext';
 import { useGlobalUserDetailsContext } from '@next-substrate/context/UserDetailsContext';
-import AddressComponent from '@next-common/ui-components/AddressComponent';
 import React, { useEffect, useState } from 'react';
 import { EditAddressModal, RemoveAddressModal } from '@next-substrate/app/components/AddressBook/AddressTable';
+import Identicon from '@polkadot/react-identicon';
+import shortenAddress from '@next-substrate/utils/shortenAddress';
+import { CopyIcon, ExternalLinkIcon } from '@next-common/ui-components/CustomIcons';
+import copyText from '@next-substrate/utils/copyText';
 
 const SingleMember = ({ name, address: memberAddress }: { name: string; address: string }) => {
 	const { activeOrg } = useActiveOrgContext();
@@ -62,11 +65,25 @@ const SingleMember = ({ name, address: memberAddress }: { name: string; address:
 	return (
 		<div className='flex items-center px-2 pb-2 mb-2 gap-x-3 text-white grid grid-cols-9'>
 			<p className='col-span-2'>{name}</p>
-			<p className='col-span-5'>
-				<AddressComponent
-					onlyAddress
-					address={memberAddress}
+			<p className='col-span-5 flex items-center gap-x-3'>
+				<Identicon
+					size={28}
+					value={memberAddress}
+					theme='polkadot'
 				/>
+				<span>{shortenAddress(memberAddress || '', 10)}</span>
+				<span className='flex items-center gap-x-2'>
+					<button onClick={() => copyText(memberAddress, true)}>
+						<CopyIcon className='hover:text-primary' />
+					</button>
+					<a
+						href={`https://www.subscan.io/account/${memberAddress}`}
+						target='_blank'
+						rel='noreferrer'
+					>
+						<ExternalLinkIcon />
+					</a>
+				</span>
 			</p>
 			<p className='col-span-2 flex items-center gap-x-3'>
 				<EditAddressModal
