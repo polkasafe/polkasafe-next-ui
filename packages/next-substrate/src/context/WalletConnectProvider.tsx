39d4b700996cea1e24b5df60f042d37650fc9170
aadarsh012 <aadarsh012@gmail.com>
2024-06-28 02:15:21 +0530

    change project id

--- packages/next-substrate/src/context/WalletConnectProvider.tsx
+++ packages/next-substrate/src/context/WalletConnectProvider.tsx
@@ -29,11 +29,11 @@ export interface IWalletConnectContext {
 
 export const initialAddMultisigContext: IWalletConnectContext = {} as IWalletConnectContext;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
 
 const web3Modal = new Web3Modal({
-	projectId: 'b75902c9d985e940612273eed0a95c84',
+	// projectId: 'b75902c9d985e940612273eed0a95c84',
+	projectId: PROJECT_ID,
 	themeMode: 'dark',
 	walletConnectVersion: 2
 });
@@ -162,7 +162,7 @@ export const WalletConnectProvider = ({ children }: { children?: ReactNode }): R
 			const _client = await Client.init({
 				// logger: DEFAULT_LOGGER,
 				// relayUrl: relayerRegion,
-				projectId: 'b75902c9d985e940612273eed0a95c84',
+				projectId: PROJECT_ID,
 				metadata: DEFAULT_APP_METADATA
 			});
 			console.log('wallet connect client', _client);
