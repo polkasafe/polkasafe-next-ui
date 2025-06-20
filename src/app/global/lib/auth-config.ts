b61e886efdc2df4f41109cb41b036b84148b1973
AleemAlam <aleem.alam@outlook.com>
2025-06-20 18:22:49 +0200

    Update authentication configuration for production environment, changing redirect URI and sign-in flow settings.

--- src/app/global/lib/auth-config.ts
+++ src/app/global/lib/auth-config.ts
@@ -8,12 +8,9 @@ const clientId = process.env.NEXT_PUBLIC_CLIENT_ID as string;
 
 export const authClient = new FutureverseAuthClient({
   clientId,
-  environment: 'staging',
-  redirectUri: 'http://localhost:3000/login',
-  signInFlow: 'popup',
-  /** Custom IDP url goes under authority */
-  authority: 'https://jen.passonline.cloud'
-
+  environment: 'production',
+  redirectUri: 'https://root.polkasafe.xyz/login',
+  signInFlow: 'redirect',
 });
 
 export const getWagmiConfig = async () => {
